d4bf4e7316c60c8c59faf798c1201b66b5276833
Abhinav Kumar <abhinavk67@gmail.com>
2023-08-29 22:50:02 +0530

    addedlogo

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,11 @@
 const Card = ({ url }) => {
   return (
-    <div>
-      <div className="mt-2 md:mt-4 lg:mt-8 shadow-md rounded-lg w-[300px]">
-        <img
-          className="rounded-t-lg w-full h-[400px] hover:scale-105 transition duration-100 cursor-pointer rounded-lg object-top object-cover"
-          src={url}
-          alt="ai-images"
-        />
-      </div>
+    <div className="mt-2 md:mt-4 lg:mt-8 shadow-md rounded-lg w-[300px]">
+      <img
+        className="rounded-t-lg w-full h-[400px] hover:scale-105 transition duration-100 cursor-pointer rounded-lg object-top object-cover"
+        src={url}
+        alt="ai-images"
+      />
     </div>
   );
 };
