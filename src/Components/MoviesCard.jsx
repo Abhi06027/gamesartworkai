eda06c06c214d6601cb530fd536ebf73522b0b9d
Abhinav Kumar <abhinavk67@gmail.com>
2023-09-03 20:37:25 +0530

    Addednewspage

--- src/Components/MoviesCard.jsx
+++ src/Components/MoviesCard.jsx
@@ -2,7 +2,7 @@ const MoviesCard = ({ title, Imgsrc }) => {
   return (
     <div>
       <div>
-        <div className="bg-white shadow-md  rounded-lg  lg:w-[600px] mx-4  ">
+        <div className="bg-white shadow-md  rounded-lg  lg:w-[500px] lg:h-[430px] h-[470px] mx-4  ">
           <img
             className="rounded-t-lg w-full h-80 object-cover"
             src={Imgsrc}
